1a5dffaa7163be9ecf8517e98f01574cc57496a0
axwhyzee <siah_weehung@yahoo.com>
2023-05-10 22:34:21 +0800

    added projection arg to find_many() and simplified clean_up_by_days()

--- app/database_handler.py
+++ app/database_handler.py
@@ -6,9 +6,8 @@ import os
 
 config = read_config('MONGODB')
 COLLECTION_NEWS = config['COLLECTION_NEWS']
-COLLECTION_NODES = config['COLLECTION_NEWS']
-COLLECTION_RELATIONS = config['COLLECTION_NEWS']
-COLLECTION_EMBEDDINGS = config['COLLECTION_NEWS']
+COLLECTION_NODES = config['COLLECTION_NODES']
+COLLECTION_RELATIONS = config['COLLECTION_RELATIONS']
 
 client = MongoClient(os.environ.get('MONGODB_URL'))
 db = client[config['DB_NAME']]
@@ -54,7 +53,7 @@ def find_all(collection: str) -> list:
 
     return list(cursor)
 
-def find_many(collection: str, condition: dict) -> list[dict]:
+def find_many(collection: str, condition: dict, projection: dict) -> list[dict]:
     '''
     Find multiple documents
 
@@ -62,7 +61,7 @@ def find_many(collection: str, condition: dict) -> list[dict]:
     :param dict condition: Match condition
     :return: List of documents that match condition
     '''
-    return db[collection].find(condition)
+    return db[collection].find(condition, projection)
 
 def delete_many(collection: str, condition) -> dict:
     '''
@@ -74,7 +73,7 @@ def delete_many(collection: str, condition) -> dict:
     '''
     return db[collection].delete_many(condition).deleted_count
 
-def clean_up_by_days(days: int) -> int:
+def clean_up_by_days(days: int):
     '''
     Remove all news documents where publish date is older than specified days old.
     Decrement frequency of nodes that were keywords of the news documents deleted.
@@ -83,37 +82,7 @@ def clean_up_by_days(days: int) -> int:
     :return: Object containing number of nodes and news documents deleted
     '''
     lower_limit = time.time() - days * 24 * 60 * 60
-    decrement_keywords = {}
-    key = ''
-    count = 0
 
-    for doc in find_many(COLLECTION_NEWS, {'datetime': {"$lt": lower_limit}}):
-        print(doc)
-        for keyword in doc['keys']:
-            decrement_keywords[keyword] = decrement_keywords.get(keyword, 0) + 1
-
-    # replace all nodes with newly calculated ones
-    nodes = find_all(COLLECTION_NODES)
-
-    for i in range(len(nodes)-1, -1, -1):
-        key = nodes[i]['data']
-        if key in decrement_keywords:
-            count = decrement_keywords[key]
-
-            # key == count means after decrementing, the node will have frequency of 0
-            if key == count:
-                nodes.pop(i)
-            else:
-                nodes[i]['freq'] -= count
-
+    delete_many(COLLECTION_NEWS, {'datetime': {"$lt": lower_limit}})
     delete_many(COLLECTION_NODES, {})
-    embeddings = list(filter(lambda doc:doc['data'] in nodes, find_all(COLLECTION_EMBEDDINGS)))
-    delete_many(COLLECTION_EMBEDDINGS, {})
-
-    if nodes:
-        insert_many(COLLECTION_NODES, nodes)
-        insert_many(COLLECTION_EMBEDDINGS, embeddings)
-
-    delete_news_count = delete_many(COLLECTION_NEWS, {'datetime': {'$lt': lower_limit}})
-
-    return {'News deleted': delete_news_count, 'Update nodes': len(decrement_keywords)}
+    delete_many(COLLECTION_RELATIONS, {})
